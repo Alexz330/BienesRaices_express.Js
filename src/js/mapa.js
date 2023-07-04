099bbe60c89ae90a69add31a2b937e31900d1639
alexz330 <alexislc333@gmail.com>
2023-07-03 21:45:52 -0600

    agregando pin al mapa de propiedades

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -3,6 +3,10 @@
     const lgn = -90.5534834;
     const map = L.map('mapa').setView([lat,lgn],16);
     let marker;
+
+    // Utilizar Provider y Geocoder
+    const geocoderService = L.esri.Geocoding.geocodeService();
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
         attribution:'&copy <a href="https://www.openstreetmap.org/copyrigth">OpenStreetMap</a> cotributors'
     }).addTo(map)
@@ -15,11 +19,18 @@
 
     // Detectar el moviento del pin
 
-    marker.on(function(e){
+    marker.on("moveend",function(e) {
         marker = e.target
+
         const posicion = marker.getLatLng();
 
-        console.log(posicion);
+        map.panTo(new L.LatLng(posicion.lat, posicion.lng));
+
+        //Obtener la informacion de las ecalles al sotra el pin
+        geocoderService.reverse().latlng(posicion,13).run(function (err, resultado){
+            
+            marker.bindPopup(resultado.address.LongLabel)
+        })
     })
 })()
 
